675c3e72e1d43fe88dcfc33e2391383942c3cffe
Bruno Tenório <tenoriobn@gmail.com>
2023-07-20 18:25:21 -0300

    Utilizando componente NavLink para verificação de rota

--- src/componentes/MenuLink/index.js
+++ src/componentes/MenuLink/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import styles from './MenuLink.module.css';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function MenuLink( { children, to } ) {
-    const localizacao = useLocation();
 
     return (
-        <Link className={`${styles.link} ${localizacao.pathname === to ? styles.linkDestacado : ""}`} to={to}>
+        <NavLink className={({ isActive }) => `${styles.link} ${isActive ? styles.linkDestacado : ""}`} to={to}>
             {children}
-        </Link>
+        </NavLink>
     )
 }
