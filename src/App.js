dddb3b16602ce0a30beb8aad1dfd09cc87ffa44e
Bruno Tenório <tenoriobn@gmail.com>
2023-07-20 16:22:26 -0300

    Criando o roteador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
-// import Inicio from "./paginas/Inicio";
-// import SobreMim from "./paginas/SobreMim";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Pagina não encontrada</div>} />
+      </Routes>
     </BrowserRouter>
   )
 }
