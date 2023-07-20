81a42579936c39f610c307b69653f0f3c3f96669
Bruno Tenório <tenoriobn@gmail.com>
2023-07-20 16:36:29 -0300

    Utilizando Link do react-router-dom

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Menu.module.css';
+import { Link } from 'react-router-dom';
+
+export default function Menu() {
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <Link className={styles.link} to="/">Início</Link>
+                <Link className={styles.link} to="/sobremim">Sobre Mim</Link>
+            </nav>
+        </header>
+    )
+}
