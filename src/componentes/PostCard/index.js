9af4f8be5bb346b9c6a79a06d79e88aea8422190
Bruno Tenório <tenoriobn@gmail.com>
2023-07-24 16:46:52 -0300

    Renomeando arquivo de estilização para PostCard

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Post.module.css'
+import styles from './PostCard.module.css'
 import { Link } from 'react-router-dom';
 import BotaoPrincipal from 'componentes/BotaoPrincipal';
 
