0228419f7d4f419ef3f4ea8782a23ec54140729c
Bruno Tenório <tenoriobn@gmail.com>
2023-07-25 10:37:48 -0300

    Implementando posts recomendados e ScrollToTop

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,4 +1,5 @@
 import './Post.css';
+import styles from './Post.module.css';
 import React from 'react';
 import { Route, Routes, useParams } from 'react-router-dom';
 import posts from "json/posts.json";
@@ -6,6 +7,7 @@ import PostModelo from 'componentes/PostModelo';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import NaoEncontrada from 'paginas/NaoEncontrada';
 import PaginaPadrao from 'componentes/PaginaPadrao';
+import PostCard from 'componentes/PostCard';
 
 export default function Post() {
     const parametros = useParams();
@@ -17,6 +19,11 @@ export default function Post() {
     if (!post) {
         return <NaoEncontrada />
     }
+
+    const postsRecomendados = posts
+        .filter((post) => post.id !== Number(parametros.id))
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 4);
 
     return (
         <Routes>
@@ -31,6 +38,16 @@ export default function Post() {
                                 {post.texto}
                             </ReactMarkdown>
                         </div>
+
+                        <h2 className={styles.tituloOutrosPosts}>Outros posts que você pode gostar</h2>
+
+                        <ul className={styles.postsRecomendados}>
+                            {postsRecomendados.map((post) => (
+                                <li key={post.id}>
+                                    <PostCard post={post} />
+                                </li>
+                            ))}
+                        </ul>
                     </PostModelo>
                 } />
             </Route>
